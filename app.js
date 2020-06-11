423486bc7bc353da45c077c80af1c32dddfd2b1d
Arpit <arpitdalal21@gmail.com>
2020-06-11 17:08:46 -0300

    added sitemap and robots.txt for good SEO

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const robots = require('express-robots-txt');
 const router = express.Router();
 const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
+app.use(
+	robots({
+		UserAgent: '*',
+		Disallow: '',
+		Sitemap: 'https://www.arpitdalal.dev/sitemap.xml'
+	})
+);
 
 router.get('', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+router.get('/sitemap.xml', (req, res) => res.sendFile(__dirname + '/sitemap.xml'));
 router.get('*', (req, res) => res.sendFile(__dirname + '/public/404.html'));
 
 app.use('/', router);
