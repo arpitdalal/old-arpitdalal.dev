883dd73e3dd256d746de115c1cbd8da45ed233ae
Arpit <arpitdalal21@gmail.com>
2020-06-11 02:29:36 -0300

    added 404 page

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
-router.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+router.get('', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+router.get('*', (req, res) => res.sendFile(__dirname + '/public/404.html'));
 
 app.use('/', router);
 app.listen(port, () => console.log(`arpitdalal.dev initialized on port ${port}!`));
