38cac27f67f44561d40ad0045024b0394ba01f72
Arpit <arpitdalal21@gmail.com>
2020-07-06 10:53:59 -0300

    re-vamped the site... it is in beta v2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const robots = require('express-robots-txt');
+const ngrok = require('ngrok');
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
@@ -14,10 +15,19 @@ app.use(
 );
 
 router.get('', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+router.get('/me', (req, res) => res.sendFile(__dirname + '/public/me.html'));
+router.get('/projects', (req, res) => res.sendFile(__dirname + '/public/projects.html'));
+router.get('/contact', (req, res) => res.sendFile(__dirname + '/public/contact.html'));
 router.get('/sitemap.xml', (req, res) => res.sendFile(__dirname + '/sitemap.xml'));
-router.get('/public/images/arpit.png', (req, res) => res.sendFile(__dirname + '/public/images/arpit.png'));
-router.get('/public/pdfs/resume.pdf', (req, res) => res.sendFile(__dirname + '/public/pdfs/resume.pdf'));
+router.get('/arpit.png', (req, res) => res.sendFile(__dirname + '/public/images/arpit.png'));
+router.get('/logo.png', (req, res) => res.sendFile(__dirname + '/public/images/arpit.png'));
+router.get('/resume', (req, res) => res.sendFile(__dirname + '/public/pdfs/resume.pdf'));
 router.get('*', (req, res) => res.sendFile(__dirname + '/public/404.html'));
 
 app.use('/', router);
-app.listen(port, () => console.log(`arpitdalal.dev initialized on port ${port}!`));
+app.listen(port, () => {
+	console.log(`local: http://localhost:${port}`);
+	(async () => {
+		console.log(`public: ${await ngrok.connect(port)}`);
+	})();
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,63 @@
-const $pdf = $('.pdf');
-const $imgDiv = $('.imgDiv');
-const $resume = $('.btn-resume');
-const $resumeDiv = $('#resume');
+const $navLinks = $('.nav-link');
+const $me = $('#me');
+const $projects = $('#projects');
+const $contact = $('#contact');
+const $resume = $('#resume');
+const $iframe = $('#iframe');
+const $toggleThemeSwitch = $('.toggle-theme-switch');
+const $cookieConsent = $('#cookie-consent');
+const $cookieOkayBtn = $('.cookie-okay');
 
-function toggleResume(event) {
-	if (!$pdf.is(':visible')) {
-		$imgDiv.hide(500);
-		$pdf.show(300);
-		$resume.html("<i class='eye slash icon'></i> Hide resume");
+$cookieOkayBtn.on('click', () => {
+	$cookieConsent.hide();
+	document.cookie = 'consent=yes';
+});
+
+$toggleThemeSwitch.click(() => {
+	$('.main').toggleClass('light-mode');
+	if ($('.main').hasClass('light-mode')) {
+		document.cookie = `theme=light`;
 	} else {
-		$pdf.hide(300);
-		$imgDiv.show(250);
-		$resume.html("<i class='eye icon'></i> Show resume");
+		document.cookie = `theme=dark`;
 	}
-}
+});
+
+// function to get cookie value
+let getCookieValue = (name) => {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+};
 
 $(document).ready(function() {
-	// Add smooth scrolling
-	$($resume).on('click', function(event) {
-		if (this.hash !== '') {
-			event.preventDefault();
-			var hash = this.hash;
+	if (!getCookieValue('consent')) {
+		// if there is a cookie named "consent" then make consent box visible
+		$cookieConsent.css('visibility', 'visible');
+	}
 
-			$('html, body').animate(
-				{
-					scrollTop: $(hash).offset().top
-				},
-				500
-			);
+	if (getCookieValue('theme')) {
+		// will set the theme on basis of the value in the cookie "theme"
+		let themeValue = getCookieValue('theme');
+		if (themeValue != 'dark') {
+			$('.main').addClass('light-mode');
+			$toggleThemeSwitch.prop('checked', true);
+		} else {
+			$toggleThemeSwitch.prop('checked', false);
 		}
+	} else {
+		// if there is no cookie name "theme", will set one up on basis of the current theme user has
+		if ($('.main').hasClass('light-mode')) {
+			document.cookie = `theme=light`;
+		} else {
+			document.cookie = `theme=dark`;
+		}
+	}
+
+	// changing the background color of the nav links on click
+	$($navLinks).on('click', function() {
+		$('.nav-link.active').removeClass('active');
+		$(this).addClass('active');
+		// setting the src of the iframe to the id of the nav-link clicked
+		$iframe.attr('src', `${$(this).attr('id')}`);
 	});
 });
