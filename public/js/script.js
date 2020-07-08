d072e53cfc764ae82602bef4780f0f6ebb7bd9c8
Arpit <arpitdalal21@gmail.com>
2020-07-08 15:23:59 -0300

    added gsap animations

--- public/js/script.js
+++ public/js/script.js
@@ -8,56 +8,57 @@ const $toggleThemeSwitch = $('.toggle-theme-switch');
 const $cookieConsent = $('#cookie-consent');
 const $cookieOkayBtn = $('.cookie-okay');
 
+let tl = gsap.timeline({ duration: 0.5 });
+tl
+  .from('.left-div', { x: -50, opacity: 0 })
+  .from('.right-div', { x: 50, opacity: 0 }, '-=0.5')
+  .from('.toggle-theme', { scale: 0, opacity: 0 })
+  .from('.anim', { y: -20, opacity: 0, stagger: 0.2 });
+
 $cookieOkayBtn.on('click', () => {
-	$cookieConsent.hide();
-	document.cookie = 'consent=yes';
+  $cookieConsent.hide();
+  document.cookie = 'consent=yes';
 });
 
 $toggleThemeSwitch.click(() => {
-	$('.main').toggleClass('light-mode');
-	if ($('.main').hasClass('light-mode')) {
-		document.cookie = `theme=light`;
-	} else {
-		document.cookie = `theme=dark`;
-	}
+  $('.main').toggleClass('light-mode');
+  if ($('.main').hasClass('light-mode')) {
+    document.cookie = `theme=light`;
+  } else {
+    document.cookie = `theme=dark`;
+  }
 });
 
-// function to get cookie value
 let getCookieValue = (name) => {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 };
 
 $(document).ready(function() {
-	if (!getCookieValue('consent')) {
-		// if there is a cookie named "consent" then make consent box visible
-		$cookieConsent.css('visibility', 'visible');
-	}
+  if (!getCookieValue('consent')) {
+    $cookieConsent.css('visibility', 'visible');
+  }
 
-	if (getCookieValue('theme')) {
-		// will set the theme on basis of the value in the cookie "theme"
-		let themeValue = getCookieValue('theme');
-		if (themeValue != 'dark') {
-			$('.main').addClass('light-mode');
-			$toggleThemeSwitch.prop('checked', true);
-		} else {
-			$toggleThemeSwitch.prop('checked', false);
-		}
-	} else {
-		// if there is no cookie name "theme", will set one up on basis of the current theme user has
-		if ($('.main').hasClass('light-mode')) {
-			document.cookie = `theme=light`;
-		} else {
-			document.cookie = `theme=dark`;
-		}
-	}
+  if (getCookieValue('theme')) {
+    let themeValue = getCookieValue('theme');
+    if (themeValue != 'dark') {
+      $('.main').addClass('light-mode');
+      $toggleThemeSwitch.prop('checked', true);
+    } else {
+      $toggleThemeSwitch.prop('checked', false);
+    }
+  } else {
+    if ($('.main').hasClass('light-mode')) {
+      document.cookie = `theme=light`;
+    } else {
+      document.cookie = `theme=dark`;
+    }
+  }
 
-	// changing the background color of the nav links on click
-	$($navLinks).on('click', function() {
-		$('.nav-link.active').removeClass('active');
-		$(this).addClass('active');
-		// setting the src of the iframe to the id of the nav-link clicked
-		$iframe.attr('src', `${$(this).attr('id')}`);
-	});
+  $($navLinks).on('click', function() {
+    $('.nav-link.active').removeClass('active');
+    $(this).addClass('active');
+    $iframe.attr('src', `${$(this).attr('id')}`);
+  });
 });
